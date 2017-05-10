f230db4a44df11afb8f53be098a163c553011196
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-10 21:18:06 +0000

    Initial commit

---
+++ express_server.js
@@ -0,0 +1,99 @@
+
+const express = require("express");
+const app = express();
+let PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let urlDatabase = {
+  "b2xvn2": "http://www.lighthouselabs.ca",
+  "9sm5xk": "http://www.google.com"
+};
+
+// function checkURLexist() {
+//   for (let i in urlDatabase) {
+//     if (!urlDatabase[i]) {
+//     res.status(404);
+//     res.send('URL not used');
+//     return;
+//     };
+//   };
+// };
+
+function generateRandomString() {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i = 0; i < 6; i += 1 ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+  return text;
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body><i>Hello</i> <b>World</b></body></html>\n")
+});
+
+// Show /url page
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// Create-new-URL-to-be-shoretened page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Generate new shortURL and redirects to that new page
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+//Go to /url/shorURL page
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id]
+                     };
+  res.render("urls_show", templateVars);
+});
+
+// Redirect immediately to the real URL set
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// Delete a url in database
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls");
+});
+
+// Edit the set URL of a shortened-url
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+
+// Listen to the stated PORT
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
